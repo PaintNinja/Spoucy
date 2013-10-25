be6d8809addb6bc35c469c3ae4cd2df68d6ecd8d
PaintSenjiNinja <paintninjamail-projects@yahoo.co.uk>
2013-10-25 22:50:13 +0100

    Time to start! :D
    Now I've got the basics setup, I shouldn't need to post commits every
    few minutes. Sorry about that :/

---
+++ src/Ninja/Spoucy/SpoucyCore.java
@@ -0,0 +1,20 @@
+package Ninja.Spoucy;
+
+import net.minecraftforge.common.EnumHelper;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+//import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+
+// Basic needed forge stuff
+@Mod(modid="Spoucy",name="Spoucy",version="v1")
+@NetworkMod(clientSideRequired=true,serverSideRequired=false)
+
+public class SpoucyCore {
+
+//Public static Item CustomItem;
+//Public static Item CustomBlock;
+
+}
