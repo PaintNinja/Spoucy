8ba76d0f5c48e8d4a8c2b2f178314aa8c25fce8f
PaintSenjiNinja <paintninjamail-projects@yahoo.co.uk>
2013-10-26 18:41:52 +0100

    Added config file, console debugging

--- src/Ninja/Spoucy/SpoucyCore.java
+++ src/Ninja/Spoucy/SpoucyCore.java
@@ -1,5 +1,6 @@
 package Ninja.Spoucy;
 
+import Ninja.Spoucy.Lib.Reference;
 import Ninja.Spoucy.API.CustomBlocks;
 import Ninja.Spoucy.API.ItemCustomBlock;
 import net.minecraft.block.Block;
@@ -16,12 +17,12 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
 // Basic needed forge stuff
-@Mod(modid="Spoucy",name="Spoucy",version="v1")
-@NetworkMod(clientSideRequired=true,serverSideRequired=false)
+@Mod(modid=Reference.MOD_ID,name=Reference.MOD_NAME,version=Reference.VERSION)
+@NetworkMod(clientSideRequired=true, serverSideRequired=false)
 
 public class SpoucyCore {
 	
-	public static final String modid = "Spoucy";
+	public static final String modid = Reference.MOD_ID;
 	
     @Instance(value = "Spoucy")
     public static SpoucyCore instance;
@@ -36,15 +37,20 @@ public class SpoucyCore {
     
     @EventHandler
     public void load(FMLInitializationEvent event) {
-		//define CustomItem
+    	System.out.println("[Spoucy] Starting...");
+		System.out.println("---------------------");
+    	//define CustomItem
+		System.out.println("[Spoucy] Defining CustomStaticItem...");
 		CustomStaticItem = new SpoucyItems(5000).setUnlocalizedName("CustomStaticItem");
-		
+		System.out.println("[Spoucy] Defined CustomStaticItem");
 		//define CustomBlock
+		System.out.println("[Spoucy] Defining CustomBlock...");
 		CustomBlock = new CustomBlocks(1000, Material.rock).setUnlocalizedName("spoucyblock").setHardness(2.0F);
 		GameRegistry.registerBlock(CustomBlock, ItemCustomBlock.class, modid + (CustomBlock.getUnlocalizedName().substring(5)));
-		
+		System.out.println("[Spoucy] Defined CustomBlock");
 		//names
-		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 0), "CustomBlock");
+		System.out.println("[Spoucy] Defining default names...");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 0), "CustomBlock1");
 		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 1), "CustomBlock2");
 		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 2), "CustomBlock3");
 		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 3), "CustomBlock4");
@@ -60,9 +66,41 @@ public class SpoucyCore {
 		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 13), "CustomBlock14");
 		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 14), "CustomBlock15");
 		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 15), "CustomBlock16");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 16), "CustomBlock17");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 17), "CustomBlock18");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 18), "CustomBlock19");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 19), "CustomBlock20");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 20), "CustomBlock21");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 21), "CustomBlock22");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 22), "CustomBlock23");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 23), "CustomBlock24");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 24), "CustomBlock25");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 25), "CustomBlock26");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 26), "CustomBlock27");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 27), "CustomBlock28");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 28), "CustomBlock29");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 29), "CustomBlock30");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 30), "CustomBlock31");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 31), "CustomBlock32");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 32), "CustomBlock33");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 33), "CustomBlock34");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 34), "CustomBlock35");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 35), "CustomBlock36");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 36), "CustomBlock37");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 37), "CustomBlock38");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 38), "CustomBlock39");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 39), "CustomBlock40");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 40), "CustomBlock41");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 41), "CustomBlock42");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 42), "CustomBlock43");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 43), "CustomBlock44");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 44), "CustomBlock45");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 45), "CustomBlock46");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 46), "CustomBlock47");
+		LanguageRegistry.addName(new ItemStack(CustomBlock, 1, 47), "CustomBlock48");
 		
 		LanguageRegistry.addName(CustomStaticItem,"Tab icon DO NOT USE");
-		
+		System.out.println("[Spoucy] Defined default names");
     }
 
 }
